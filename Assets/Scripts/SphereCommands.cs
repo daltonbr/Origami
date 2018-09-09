912e097794a133cb798240c9b65958fd26ffe212
Dalton Lima <daltonvarussa@gmail.com>
2018-09-09 20:04:10 -0300

    add user interaction with the Spheres

---
+++ Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class SphereCommands : MonoBehaviour, IFocusable, IInputHandler
+{    
+
+    void IFocusable.OnFocusEnter()
+    {
+        Debug.Log("OnFocusEnter");        
+    }
+
+    void IFocusable.OnFocusExit()
+    {
+        Debug.Log("OnFocusExit");
+    }
+
+    void IInputHandler.OnInputDown(InputEventData eventData)
+    {
+        Debug.Log("OnInputDown");
+    }
+
+    void IInputHandler.OnInputUp(InputEventData eventData)
+    {
+        Debug.Log("OnInputUp");
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }    
+
+}
